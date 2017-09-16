07992318fc02852c602135a7bf78455c8e15fcda
salva <salva@192.168.1.106>
2017-09-16 11:13:42 +0200

    commit per creare progetto su git

---
+++ src/it/polito/tdp/quadrato_magico/QuadratoMagico.java
@@ -0,0 +1,16 @@
+package it.polito.tdp.quadrato_magico;
+
+public class QuadratoMagico {
+	
+	
+	int[][] quadrato = new int [4][4];
+	private final int MAGIC_NUMBER = (4*((4^2)+1))/2;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+}
