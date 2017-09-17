2e46a66e30978eb70c7f905e4e25cda3e28fa48b
salva <salva@192.168.1.102>
2017-09-17 15:12:31 +0200

    prima versione funzionanete anche se lenta per dimensione quadrato > 3

--- src/it/polito/tdp/quadrato_magico/QuadratoMagico.java
+++ src/it/polito/tdp/quadrato_magico/QuadratoMagico.java
@@ -1,16 +1,184 @@
 package it.polito.tdp.quadrato_magico;
 
 public class QuadratoMagico {
-	
-	
-	int[][] quadrato = new int [4][4];
-	private final int MAGIC_NUMBER = (4*((4^2)+1))/2;
-	
+
+	private int magicNumber;
+	private int size;
+
+	/**
+	 * @param quadrato
+	 * @param magicNumber
+	 */
+	public QuadratoMagico(int size) {
+		this.size = size;
+		this.magicNumber = (size * ((size * size) + 1)) / 2;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		QuadratoMagico qm = new QuadratoMagico(4);
 
-		
-		
+		int[][] quadrato = qm.risolvi();
+		qm.stampaSoluzione(quadrato);
+
+	}
+
+	private int[][] risolvi() {
+
+		// soluzione madre di livello 0
+		int[][] quadratoParziale = new int[size][size];
+		int[][] quadratoBest = new int[size][size];
+		scegli(quadratoParziale, 1, quadratoBest);
+
+	
+		return quadratoBest;
+
+	}
+
+	private void scegli(int[][] quadratoParziale, int livello, int[][] quadratoBest) {
+
+		// livello = size^2 e somma colonne, righe e diagonali = numero magico
+		// salva in quadrato best
+		// stampa soluzione
+		if (livello == (size*size)+1) {
+			if (sommeRCDMagiche(quadratoParziale)) {
+				valorizzaEStampaSoluzione(quadratoParziale, quadratoBest);
+			}
+		}
+		else {
+		//	stampaSoluzione(quadratoParziale);
+
+			// quadrato parziale non pieno
+			for (int numero = 1; numero <= (size*size); numero++) {
+				if (!giaInserito(quadratoParziale, numero)) {
+					// il numero i non c'è ancora, provo a metterlo
+					// se somme righe, colonne e diagonali non superno numero
+					// magico
+					if (!sommeRCDSforate(quadratoParziale)) {
+					
+						//System.out.println("<i - livello> " + numero + " - " + livello);
+						
+						// prova a mettere i nel quadrato
+						quadratoParziale[(livello-1) / (size)][((livello-1) % (size))] = numero;
+						// e delegare la ricerca al livello successivo
+						scegli(quadratoParziale, livello + 1, quadratoBest);
+						// poi rimetti le cose a posto (togli i)
+						quadratoParziale[(livello-1) / (size)][((livello-1) % (size))] = 0;
+					}
+				}
+			}
+		}
+
+	}
+
+	private void valorizzaEStampaSoluzione(int[][] quadratoParziale, int[][] quadratoBest) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				quadratoBest[i][j] = quadratoParziale[i][j];
+		//		System.out.println(quadratoBest[i][j] + "\t");
+			}
+		//	System.out.println("\n");
+
+		}
+
+	}
+
+	private void stampaSoluzione(int[][] quadrato) {
+		StringBuffer sb;
+		for (int i = 0; i < size; i++) {
+			sb = new StringBuffer();
+			for (int j = 0; j < size; j++) {
+				sb.append(quadrato[i][j]).append(" ");
+			}
+			System.out.println(sb.toString());
+
+		}
+
+	}
+
+	private boolean sommeRCDMagiche(int[][] quadratoParziale) {
+		int[] parziali = sommeRCD(quadratoParziale);
+
+		for (int i = 0; i < (size*2)+2; i++) {
+//			System.out.println("<sommeRCDMagiche> parziali[i]="+parziali[i]);
+			if (parziali[i] != magicNumber) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
+
+	private int[] sommeRCD(int[][] quadratoParziale) {
+		int[] parziali = new int[(size * 2) + 2];
+
+		// righe
+		int sommaR;
+		// colonne
+		int sommaC;
+		// diagonale principale
+		int sommaD;
+		// diagonale inversa
+		int sommaI;
+
+		for (int i = 0; i < size; i++) {
+			sommaR = 0;
+			sommaC = 0;
+			for (int j = 0; j < size; j++) {
+				sommaR += quadratoParziale[i][j];
+				sommaC += quadratoParziale[j][i];
+			}
+			parziali[2 * i] = sommaR;
+			parziali[(2 * i) + 1] = sommaC;
+
+		}
+
+		// diagonali
+		sommaD = 0;
+		sommaI = 0;
+		for (int i = 0; i < size; i++) {
+			sommaD += quadratoParziale[i][i];
+			sommaI += quadratoParziale[i][size - i - 1];
+		}
+		parziali[(size * 2)] = sommaD;
+		parziali[(size * 2)+1] = sommaI;
+
+		return parziali;
+	}
+
+	/**
+	 * verifica se la somma dei numeri sulle diagonali, righe e colonne
+	 * aggiungendo il numero passato in input non ha superato il numero magico
+	 * 
+	 * @param quadratoParziale
+	 * @param numero
+	 * @return
+	 */
+	private boolean sommeRCDSforate(int[][] quadratoParziale) {
+
+		int[] parziali = sommeRCD(quadratoParziale);
+
+		for (int i = 0; i < (size*2)+2; i++) {
+//			System.out.println("<sommeRCDSforate> parziali[i]="+parziali[i]);
+			if (parziali[i] > magicNumber) {
+				return true;
+			}
+		}
+
+		return false;
+
+	}
+
+	private boolean giaInserito(int[][] quadratoParziale, int numero) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (quadratoParziale[i][j] == numero) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 }
